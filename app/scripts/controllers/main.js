42b657ae18c406e64ec60ac9e12b564ceae39c99
Allen Nguyen <allendewy@gmail.com>
2016-01-17 19:25:43 -0700

    Set up new environment with Yeoman.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name allenduymeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the allenduymeApp
+ */
+angular.module('allenduymeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
