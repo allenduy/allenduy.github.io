bfd8db0d71b5a8dd91aac30d316852f8f008e0f5
Allen Nguyen <allendewy@gmail.com>
2016-02-28 23:16:49 -0700

    Added contact, portfolio, and resume controllers, views, and default tests.

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name allenduymeApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the allenduymeApp
+ */
+angular.module('allenduymeApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/resume.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name allenduymeApp.controller:ResumeCtrl
+ * @description
+ * # ResumeCtrl
+ * Controller of the allenduymeApp
+ */
+angular.module('allenduymeApp')
+  .controller('ResumeCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
